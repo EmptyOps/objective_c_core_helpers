62bd23d1cd395166e39b66d33c04a985357b91e0
Hiren <ajay@emptyops.com>
2019-03-04 13:17:27 +0530

    last commit 4_02_2019 1:17 PM

--- Example/Pods/Target Support Files/Pods-objective_c_core_helpers_Example/Pods-objective_c_core_helpers_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-objective_c_core_helpers_Example/Pods-objective_c_core_helpers_Example-frameworks.sh
@@ -143,9 +143,21 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ISO8601DateFormatterValueTransformer/ISO8601DateFormatterValueTransformer.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RKValueTransformers/RKValueTransformers.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RestKit/RestKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SOCKit/SOCKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/TransitionKit/TransitionKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/objective_c_core_helpers/objective_c_core_helpers.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ISO8601DateFormatterValueTransformer/ISO8601DateFormatterValueTransformer.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RKValueTransformers/RKValueTransformers.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RestKit/RestKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SOCKit/SOCKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/TransitionKit/TransitionKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/objective_c_core_helpers/objective_c_core_helpers.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
